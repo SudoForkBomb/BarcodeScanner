17e94c84572bcb5f52c90e156e52f1a7211263c5
Chris Taylor <crtaylor@g.cofc.edu>
2016-10-21 21:29:57 -0400

    Initial commit

---
+++ app/src/main/java/com/crtaylor123/barcodescanner/MainActivity.java
@@ -0,0 +1,59 @@
+package com.crtaylor123.barcodescanner;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+        TextView txtView = (TextView) findViewById(R.id.txtContent);
+
+        BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext()).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
+
+        /*
+        You must provide a TrackerFactory to create a new Tracker instance for each barcode.
+        For this example, BarcodeTrackerFactory creates a BarcodeGraphic that overlays the
+          bounding box and value for each barcode result:
+         */
+        BarcodeTrackerFactory barcodeTrackerFactory = new BarcodeTra
+        if (!detector.isOperational()){
+            txtView.setText("Could not set up the detector!");
+            return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+        Barcode thisCode = barcodes.valueAt(0);
+        txtView.setText(thisCode.rawValue);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
