435c2d560f47d2be51fcb09bf25f0de1967e85f2
Chris Taylor <chrisrtaylor123@gmail.com>
2017-01-12 11:59:44 -0500

    Adjusted the layout by making the cameraview smaller, so you could see more of the textview. Changed the retrofit call to make asynchronous calls, but it seems to be failing. More troubleshooting needed.

--- app/src/main/java/com/crtaylor123/barcodescanner/BarcodeRetrofit.java
+++ app/src/main/java/com/crtaylor123/barcodescanner/BarcodeRetrofit.java
@@ -1,14 +1,8 @@
 package com.crtaylor123.barcodescanner;
 
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,9 +12,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BarcodeRetrofit {
     static final String apiKey = "7cc78acb320c8441dcd662d0e014f112";
+    String upcString = "Not Working";
 
     public String getBarcodeInfo(String barcodeInfo) {
 
+        barcodeInfo = "0111222333446";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.upcdatabase.org/json/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -29,8 +25,25 @@ public class BarcodeRetrofit {
         BarcodeAPI barcodeAPI = retrofit.create(BarcodeAPI.class);
         System.out.println(barcodeInfo);
 //        barcodeAPI.getBarcodeAPI(barcodeInfo, apiKey);
-        //Call<UPC> upc = barcodeAPI.getBarcodeAPI(apiKey, barcodeInfo);
-        Call<UPC> upc = barcodeAPI.getBarcodeAPITWO(barcodeInfo);
-        return upc.toString();
+        //Call<UPC> call = barcodeAPI.getBarcodeAPI(apiKey, barcodeInfo);
+        Call<UPC> call = barcodeAPI.getBarcodeAPITWO(barcodeInfo);
+        call.enqueue(new Callback<UPC>() {
+            @Override
+            public void onResponse(Call<UPC> call, Response<UPC> response) {
+                int statusCode = response.code();
+                UPC upcResults = response.body();
+                upcResults.toString();
+            }
+
+            @Override
+            public void onFailure(Call<UPC> call, Throwable t) {
+                upcString = t.toString();
+            }
+        });
+        System.out.println(call.toString());
+
+        return upcString;
     }
+
+
 }
